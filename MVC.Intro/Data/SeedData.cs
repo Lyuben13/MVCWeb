543ad6069ee7b67040e1b63f2e52cc34e25d4bb1
LyubenAndreev <liuben.Andreev@gmail.com>
2025-10-23 11:12:32 +0300

    Changes

--- MVC.Intro/Data/SeedData.cs
+++ MVC.Intro/Data/SeedData.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using MVC.Intro.Models;
 
 namespace MVC.Intro.Data
@@ -6,55 +10,60 @@ namespace MVC.Intro.Data
     {
         public static async Task SeedAsync(AppDbContext context)
         {
-            if (!context.Products.Any())
-            {
-                var products = new List<Product>
-                {
-                    new Product
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = "Gaming Laptop",
-                        Price = 2500.00m,
-                        Description = "Мощен лаптоп за гейминг и работа",
-                        ImagePath = "images/laptop.svg",
-                        Category = "Електроника",
-                        InStock = true
-                    },
-                    new Product
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = "Smartphone",
-                        Price = 800.00m,
-                        Description = "Модерен смартфон с най-новите функции",
-                        ImagePath = "images/phone.svg",
-                        Category = "Електроника",
-                        InStock = true
-                    },
-                    new Product
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = "Programming Book",
-                        Price = 45.00m,
-                        Description = "Книга за програмиране на C# и ASP.NET",
-                        ImagePath = "images/book.svg",
-                        Category = "Книги",
-                        InStock = true
-                    },
-                    new Product
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = "Coffee Mug",
-                        Price = 15.00m,
-                        Description = "Керамична чаша за кафе с лого",
-                        ImagePath = "images/product-placeholder.svg",
-                        Category = "Други",
-                        InStock = true
-                    }
-                };
+            // Създава базата, ако още не е създадена
+            await context.Database.EnsureCreatedAsync();
 
-                context.Products.AddRange(products);
-                await context.SaveChangesAsync();
-            }
+            // Проверява дали вече има продукти
+            if (context.Products.Any())
+                return;
+
+            // Примерни начални продукти
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Gaming Laptop",
+                    Price = 2500.00m,
+                    Description = "Мощен лаптоп за гейминг и работа",
+                    ImagePath = "images/laptop.svg",
+                    Category = "Електроника",
+                    InStock = true
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Smartphone",
+                    Price = 800.00m,
+                    Description = "Модерен смартфон с най-новите функции",
+                    ImagePath = "images/phone.svg",
+                    Category = "Електроника",
+                    InStock = true
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Programming Book",
+                    Price = 45.00m,
+                    Description = "Книга за програмиране на C# и ASP.NET",
+                    ImagePath = "images/book.svg",
+                    Category = "Книги",
+                    InStock = true
+                },
+                new Product
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Coffee Mug",
+                    Price = 15.00m,
+                    Description = "Керамична чаша за кафе с лого",
+                    ImagePath = "images/product-placeholder.svg",
+                    Category = "Други",
+                    InStock = true
+                }
+            };
+
+            await context.Products.AddRangeAsync(products);
+            await context.SaveChangesAsync();
         }
     }
 }
